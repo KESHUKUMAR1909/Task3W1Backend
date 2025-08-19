f1077f9ea7d6389296f9491873be92531e915553
KESHUKUMAR1909 <146375035+KESHUKUMAR1909@users.noreply.github.com>
2025-08-19 15:38:35 +0530

    Update seedUser.js
    Removing the mongoDb link

--- seedUser.js
+++ seedUser.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const User = require("./models/User");
 
-mongoose.connect("mongodb+srv://keshukumar1909:8810630470@cluster0.r4h3z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("")
   .then(async () => {
     const users = ["Rahul", "Kamal", "Sanak", "Ravi", "Sita", "Anita", "Karan", "Neha", "Amit", "Priya"];
     for (let name of users) {
